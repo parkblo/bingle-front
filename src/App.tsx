f55c55a1f75a6f376ef0f7bd5f00668de59c9972
Byeongju Park <badmuxi@gmail.com>
2024-04-27 22:58:43 +0900

    ✨ feat: 지도 객체 생성 #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Container, NaverMap, NavermapsProvider } from "react-naver-maps";
+
 import "./App.css";
 import "./global.css";
+import secret from "./secret";
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <NavermapsProvider ncpClientId={secret}>
+        <h1>hello naver maps</h1>
+        <Container style={{ width: "100%", height: "100vh" }}>
+          <NaverMap></NaverMap>
+        </Container>
+      </NavermapsProvider>
+    </div>
+  );
 }
 
 export default App;
