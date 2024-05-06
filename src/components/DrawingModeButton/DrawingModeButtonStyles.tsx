3f3ce79ac4267d3bbdd34d1a32c31da92583af75
Byeongju Park <badmuxi@gmail.com>
2024-05-06 22:56:00 +0900

    ✨ feat: 드로잉 버튼 클릭시 스타일 변경 #5

--- src/components/DrawingModeButton/DrawingModeButtonStyles.tsx
+++ src/components/DrawingModeButton/DrawingModeButtonStyles.tsx
@@ -18,6 +18,23 @@ const styles = {
       backgroundColor: colors.main,
     },
   },
+  activeButtonContainer: {
+    height: "100px",
+    width: "100px",
+    position: "absolute",
+    bottom: "40px",
+    right: "40px",
+    backgroundColor: colors.sub,
+    borderRadius: "90px",
+    boxShadow: "7",
+    zIndex: "1",
+    "&:hover": {
+      backgroundColor: colors.sub,
+    },
+    ".MuiTouchRipple-child": {
+      backgroundColor: colors.main,
+    },
+  },
   image: {
     height: "70px",
     width: "70px",
