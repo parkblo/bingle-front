ffca75d05624d1da7d3a3ac988799828cb176d38
Byeongju Park <badmuxi@gmail.com>
2024-05-08 03:01:54 +0900

    ✨ feat: 드로잉 툴에 정보창 표시 구현 #5

---
+++ src/components/DrawingTool/DrawingToolStyles.tsx
@@ -0,0 +1,37 @@
+let styles = {
+  informationContainer: {
+    borderRadius: "15px",
+    backgroundColor: "white",
+    width: "140px",
+    height: "50px",
+    position: "absolute",
+    zIndex: "1",
+    boxShadow: "7",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "12px",
+    flexDirection: "column",
+  },
+  radiusContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  labelText: { fontSize: "12px" },
+  inputArea: {
+    width: "80px",
+    fontSize: "20px",
+    marginLeft: "10px",
+    marginRight: "10px",
+  },
+  buttonContainer: {
+    marginTop: "10px",
+  },
+  button: {
+    height: "20px",
+  },
+};
+
+export default styles;
--- src/components/Store/Store.tsx
+++ src/components/Store/Store.tsx
@@ -2,12 +2,16 @@ import { create } from "zustand";
 
 interface Store {
   drawingMode: boolean;
+  radiusData: number;
   setDrawingMode: (mode: boolean) => void;
+  setRadiusData: (radiusData: number) => void;
 }
 
 const useStore = create<Store>((set) => ({
   drawingMode: false,
+  radiusData: 0,
   setDrawingMode: (mode: boolean) => set({ drawingMode: mode }),
+  setRadiusData: (radius: number) => set({ radiusData: radius }),
 }));
 
 export default useStore;
