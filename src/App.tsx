e7f3f226404681add3b43187a699722d27fce1bd
Byeongju Park <badmuxi@gmail.com>
2024-05-08 18:24:00 +0900

    🎨 refactor: 클라이언트 ID 환경 변수 사용

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { NavermapsProvider } from "react-naver-maps";
 
 import "./App.css";
 import "./global.css";
-import secret from "./secret";
 import MapPage from "./components/MapPage/MapPage";
 
 function App() {
   return (
     <div className="App">
-      <NavermapsProvider ncpClientId={secret} submodules={["geocoder"]}>
+      <NavermapsProvider
+        ncpClientId={process.env.REACT_APP_NAVER_CLIENT_ID || ""}
+        submodules={["geocoder"]}
+      >
         <MapPage />
       </NavermapsProvider>
     </div>
