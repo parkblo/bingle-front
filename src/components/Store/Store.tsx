b031035bb5d5d7744c40a52227542e8fe48243da
Byeongju Park <badmuxi@gmail.com>
2024-05-08 20:26:58 +0900

    ✨ feat: 사용자 설정으로 Request Body 구성 #11

--- src/components/Store/Store.tsx
+++ src/components/Store/Store.tsx
@@ -2,20 +2,33 @@ import { create } from "zustand";
 
 interface Store {
   drawingMode: boolean;
-  radiusData: number;
   dashboardOpen: boolean;
+  radiusData: number;
+  dataType: string;
+  topN: number;
+  centerCoord: { latitude: number; longitude: number };
   setDrawingMode: (mode: boolean) => void;
   setRadiusData: (radiusData: number) => void;
   setDashboardOpen: (open: boolean) => void;
+  setDataType: (dataType: string) => void;
+  setTopN: (topN: number) => void;
+  setCenterCoord: (coord: { latitude: number; longitude: number }) => void;
 }
 
 const useStore = create<Store>((set) => ({
   drawingMode: false,
   dashboardOpen: false,
   radiusData: 0,
+  dataType: "NACH_R400M",
+  topN: 100,
+  centerCoord: { latitude: 0, longitude: 0 },
   setDrawingMode: (mode: boolean) => set({ drawingMode: mode }),
   setRadiusData: (radius: number) => set({ radiusData: radius }),
   setDashboardOpen: (open: boolean) => set({ dashboardOpen: open }),
+  setDataType: (dataType: string) => set({ dataType: dataType }),
+  setTopN: (topN: number) => set({ topN: topN }),
+  setCenterCoord: (coord: { latitude: number; longitude: number }) =>
+    set({ centerCoord: coord }),
 }));
 
 export default useStore;
